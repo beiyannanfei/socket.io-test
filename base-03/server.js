b32e1b539846838434b7ef8a605597ed9003204f
wangyanqiang <wangyanqiang@sensoro.com>
2017-07-10 15:31:24 +0800

    update

--- base-03/server.js
+++ base-03/server.js
@@ -25,6 +25,7 @@ io.on('connection', function (socket) {
 			// console.log("========== rooms-key: %j, rooms: %j", Object.keys(socket.rooms), socket.rooms);
 		});
 	}
+	// console.log("========= socket.id: %j", socket.id);
 	//断开事件
 	socket.on('disconnect', function (data) {
 		console.log(`${socket.handshake.query.uid} disconnect ${data}`);
@@ -42,11 +43,24 @@ app.get("/emit", function (req, res) {
 	return res.send("ok");
 });
 
+app.get("/client", function (req, res) {
+	let rid = req.query.rid;
+	let a = io.sockets.adapter.rooms[rid];
+	console.log("===", a);
+	for (let sid in a.sockets) {
+		console.log("========== " + sid);
+		io.sockets.sockets[sid].join("cccc");
+	}
+	return res.send("OK--");
+});
+
 /*
-1).向所有客户端广播：socket.broadcast.emit('broadcast message');
-2).进入一个房间（非常好用！相当于一个命名空间，可以对一个特定的房间广播而不影响在其他房间或不在房间的客户端）：socket.join('your room name');
-3).向一个房间广播消息（发送者收不到消息）：socket.broadcast.to('your room name').emit('broadcast room message');
-4).向一个房间广播消息（包括发送者都能收到消息）（这个API属于io.sockets）：io.sockets.in('another room name').emit('broadcast room message');
-5).强制使用WebSocket通信：（客户端）socket.send('hi')，（服务器）用socket.on('message', function(data){})来接收。
-Socket.IO的进阶用法介绍基本就到这里。个人感觉在日常使用的时候这些基本API已经够用了，这也体现了Socket.IO极其简洁易用的设计哲学。本文只是抛砖引玉，当在实际运用中遇到解决不了的问题时，再去查看官方详细的WIKI会比较好。
-*/
+ 1).向所有客户端广播：socket.broadcast.emit('broadcast message');
+ 2).进入一个房间（非常好用！相当于一个命名空间，可以对一个特定的房间广播而不影响在其他房间或不在房间的客户端）：socket.join('your room name');
+ 3).向一个房间广播消息（发送者收不到消息）：socket.broadcast.to('your room name').emit('broadcast room message');
+ 4).向一个房间广播消息（包括发送者都能收到消息）（这个API属于io.sockets）：io.sockets.in('another room name').emit('broadcast room message');
+ 5).强制使用WebSocket通信：（客户端）socket.send('hi')，（服务器）用socket.on('message', function(data){})来接收。
+ Socket.IO的进阶用法介绍基本就到这里。个人感觉在日常使用的时候这些基本API已经够用了，这也体现了Socket.IO极其简洁易用的设计哲学。本文只是抛砖引玉，当在实际运用中遇到解决不了的问题时，再去查看官方详细的WIKI会比较好。
+ */
+// 深圳市亿威尔信息技术股份有限公司
+// 91440300723021032L
